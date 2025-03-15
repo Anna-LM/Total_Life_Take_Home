24175dace350d45586f4b8403c57ceefbd5c8c63
Anna-LM <annallambiasmaw@gmail.com>
2025-03-15 16:34:28 -0600

    testing delete method on database object

--- SQLite_Database.py
+++ SQLite_Database.py
@@ -12,12 +12,23 @@ class SQLiteDatabase:
         self.c.execute(f'CREATE TABLE IF NOT EXISTS {table_name} ({table_columns_information})')
         self.conn.commit()
     
-    #Adding entities/rows to the table <<table_name>>
+    # -- CRUD --
+        
+    #Create - Adding entities/rows to the table <<table_name>>
     def add_entity(self,table_name,column,value):
         self.c.execute(f'INSERT OR IGNORE INTO {table_name} ({column}) VALUES ({value})')
+        self.conn.commit()
+    
+    #Read - Reading entities from the table <<table_name>> based on a condition <<read_condition>>
+    #Update - Updateing entities from the table <<table_name>> based on a condition <<update_condition>>
+    
+    #Delete - Deleting entities from the table <<table_name>> based on a condition <<delete_condition>>
+    def delete_entity(self,table_name,delete_condition):
+        self.c.execute(f'DELETE FROM {table_name} WHERE {delete_condition}')
         self.conn.commit()
     
     #Closing the database, this must be done every time the database is accessed
     def close_database(self):
         self.conn.close()
+
 
