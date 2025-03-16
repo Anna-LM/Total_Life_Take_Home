1dd280580a31e8dfb55bcee2ff3e54282039254a
Anna-LM <annallambiasmaw@gmail.com>
2025-03-15 18:13:53 -0600

    created post request on appointment table

--- main.py
+++ main.py
@@ -4,17 +4,28 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 #Basic get response
-@app.route('/appointments', methods=['GET'])
+@app.route('/appointments', methods=['GET','POST'])
 def return_appointments():
     appointment_id = request.args.get('appointment_id')
+    appt_patient_id = request.args.get('appt_patient_id')
+    appt_clinician_id = request.args.get('appt_clinician_id')
+    date_time = request.args.get('date_time')
+    status = request.args.get('status')
 
     #open database <<DATABASE_NAME>>
     DATABASE_NAME = 'Total_Life_DB'
     active_database = SQLiteDatabase(DATABASE_NAME)
 
-    #Read the correct data
-    returned_entites = active_database.search_table('*','appointment_table',f'appointment_id = {appointment_id}','date_time DESC',None, None)
+    if request.method == 'GET':
+        #Read the correct data
+        returned_entites = active_database.search_table('*','appointment_table',f'appointment_id = {appointment_id}','date_time DESC',None, None)
     
+    elif request.method == 'POST':
+        #create new entity in appointments table
+        active_database.add_entity('appointment_table',"'appt_patient_id', 'appt_clinician_id', 'date_time','status'",f"'{appt_patient_id}', '{appt_clinician_id}', '{date_time}','{status}'")
+        returned_entites = 'Added'
+
+
     #Closing the database
     active_database.close_database()
 
